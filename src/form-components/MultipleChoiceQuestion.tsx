56bb11ee342c9fb8d839f530cae3c8137d627b67
Luke Kakad <lukek@udel.edu>
2026-03-11 19:57:40 -0400

    now we good?

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
-import { CheckAnswer } from "./CheckAnswer";
 
 export function MultipleChoiceQuestion({
     options,
